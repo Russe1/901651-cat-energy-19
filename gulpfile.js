eec760fdb401af01bb7e882a82873fa96542ee25
Russe1 <46652738+Russe1@users.noreply.github.com>
2020-05-26 21:56:41 +0300

    Резервная копия перед созданием спрайта

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ var imagemin = require("gulp-imagemin");
 var del = require("del");
 var server = require("browser-sync").create();
 var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include")
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -55,6 +58,23 @@ gulp.task("webp", function() {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function(){
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
 });
 
 gulp.task("copy", function() {
